ce12193c762dbd03b9363fa23db799e4eacf7081
NeaPieroelie <nea.pieroelie@gmail.com>
2024-11-29 11:02:02 -0800

    added a bg image for main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,39 @@
 "use client";
 
 import Header from "./components/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen">
+    <>
+      <Head>
+        <title>Nea Pieroelie</title>
+        <meta name="description" content="Nea Pieroelies Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      {/* Header */}
-      <div className="sticky top-0 z-10">
-        <Header />
-      </div>
+      {/* Main Wrapper with Background Image */}
+      <div
+        className="relative min-h-screen bg-cover bg-center bg-no-repeat"
+        style={{
+          backgroundImage: "url('/background.jpg')", // Replace with your image path
+        }}
+      >
+        {/* Header */}
+        <div className="sticky top-0 z-10">
+          <Header />
+        </div>
 
-      {/* Main Content */}
-      <div className="relative flex flex-col justify-center items-center h-[655px]">
-        <h1 className="font-instrument text-massive tracking-tighter top-3 leading-26 text-white">
-          Nea Pieroelie
-        </h1>
-        <p className="font-instrument text-white text-5xl">
-          UI / UX Designer and Developer
-        </p>
+        {/* Main Content */}
+        <div className="relative flex flex-col justify-center items-center h-[655px]">
+          <h1 className="font-instrument text-massive tracking-tighter leading-26 text-white">
+            Nea Pieroelie
+          </h1>
+          <p className="font-instrument text-white text-5xl">
+            UI / UX Designer and Developer
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
