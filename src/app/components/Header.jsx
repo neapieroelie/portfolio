3baf93833001e09c2bd62c88a6d3506b8e65fa01
NeaPieroelie <nea.pieroelie@gmail.com>
2025-03-27 22:56:32 -0700

    created a little hover animation for the logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,28 +1,41 @@
 "use client";
 
+import { useState } from "react";
 import MenuButton from "./MenuButton";
 import Image from "next/image";
 
 export default function Header () {
-    return (
-        <>
-            <div className="flex flex-row items-center justify-between w-screen h-[10vh] px-10 pt-4 pb-2 bg-[#fdfcfc]">
-                {/* SVG Logo */}
-                <div className="flex justify-start">
-                    <a href="/">
-                        <Image 
-                            src="/logo_pink.svg" 
-                            alt="Logo" 
-                            width={56} 
-                            height={56} 
-                        />
-                    </a>
-                </div>
+  const [isHovered, setIsHovered] = useState(false);
 
-                <div>
-                    <MenuButton />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div className="flex flex-row items-center justify-between w-screen h-[10vh] px-10 pt-4 pb-2 bg-[#fdfcfc]">
+      
+      {/* Logo with hover image swap */}
+      <div className="flex justify-start">
+        <a
+          href="/"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <div
+            className={`relative transition-all duration-300 ${
+              isHovered ? "w-16 h-16" : "w-16 h-16"
+            }`}
+          >
+            <Image
+              src={isHovered ? "/monkeystandlogo.svg" : "/logo_pink.svg"}
+              alt="Logo"
+              fill
+              className="object-contain"
+            />
+          </div>
+        </a>
+      </div>
+
+      {/* Menu */}
+      <div>
+        <MenuButton />
+      </div>
+    </div>
+  );
 }
